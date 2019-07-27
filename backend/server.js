41458db98a4384c5c06442f22d2cefe079446152
Matt Creekmore <matthewacreekmore@gmail.com>
2019-07-27 10:04:58 -0400

    small server changes

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 
 app.use(cors());
 
-app.get('/', function (req, res) {
- return res.send('Hello world');
+PORT = process.env.PORT || 8080;
+
+app.get("/", function(req, res) {
+  return res.send("Hello world");
 });
 
-console.log('Listening on Port 8080');
-app.listen(process.env.PORT || 8080);
+console.log(`Listening on port ${PORT}`);
+app.listen(PORT);
